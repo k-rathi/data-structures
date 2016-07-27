ecafde5eddc10e1f7ac0b6425fd8fb4455b98e86
k-rathi <kunalrathi1994@gmail.com>
2016-07-27 15:44:54 -0700

    Finished Stack part 2

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
+  var newQueue = {};
+  newQueue.len = 0;
+  newQueue.storage = {};
+  _.extend(newQueue, queueMethods);
+  return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+queueMethods = {};
+
+queueMethods.size = function() {
+    return this.len;
+};
 
 
+// var someInstance = {};
+//   var len = 0;
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+
+//   // Implement the methods below
+
+//   someInstance.enqueue = function(value) {
+//     storage[len] = value;
+//     len ++;
+
+//   };
+
+//   someInstance.dequeue = function() {
+//     var temp = storage[0];
+//     for (var i = 0; i < len - 1; i++) {
+//       storage[i] = storage[i + 1];
+//     }
+//     len --;
+//     return temp;
+//   };
+
+//   someInstance.size = function() {
+//     return (len < 0) ? 0 : len;
+//   };
+
+//   return someInstance;
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,53 @@
 var Stack = function() {
+  var newStack = {};
+  newStack.len = 0;
+  newStack.storage = {};
+  _.extend(newStack, stackMethods);
+  return newStack;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return ( this.len < 0 ) ? 0 : this.len;
+};
 
+stackMethods.push = function(value) {
+  this.storage[this.len] = value;
+  this.len++;
+};
+
+stackMethods.pop = function() {
+  var temp = this.storage[this.len - 1];
+  this.len--;
+  delete this.storage[this.len];
+  return temp;
+};
+// var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var len = 0; 
+//   // Implement the methods below
+//   someInstance.push = function(value) {
+//     storage[len] = value;
+//     len++;
+//   };
+
+//   someInstance.pop = function() {
+//     var temp = storage[len - 1];
+//     delete storage.len;
+//     len--;
+//     return temp;
+//   };
+
+//   someInstance.size = function() {
+//     if (len < 0) {
+//       return 0;
+//     }
+//     return len;
+//   };
+
+//   return someInstance;
