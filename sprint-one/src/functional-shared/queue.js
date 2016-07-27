a5f9fe6aa5a0e696aeb9e37e6241763edbeeb70a
k-rathi <kunalrathi1994@gmail.com>
2016-07-27 15:54:25 -0700

    finished functional-shared pattern queue and stack.js with all tests passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,16 +2,31 @@ var Queue = function() {
   var newQueue = {};
   newQueue.len = 0;
   newQueue.storage = {};
+
   _.extend(newQueue, queueMethods);
   return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-queueMethods = {};
+var queueMethods = {};
 
 queueMethods.size = function() {
-    return this.len;
+  return (this.len > -1) ? this.len : 0;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.len] = value;
+  this.len++;
+};
+
+queueMethods.dequeue = function() {
+  var temp = this.storage[0];
+  for (var i = 0; i < this.len - 1; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  this.len--;
+  return temp;
 };
 
 
