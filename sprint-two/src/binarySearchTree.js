3bfea19fe9fc74a4c676c415c37f1b56791d6e9f
k-rathi <kunalrathi1994@gmail.com>
2016-07-28 15:49:57 -0700

    Completed Sprint 2 Base Requirements.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,43 @@
 var BinarySearchTree = function(value) {
+  var binaryTreeInstance = Object.create(binaryMethods);
+  binaryTreeInstance.value = value;
+  binaryTreeInstance.left = null;
+  binaryTreeInstance.right = null;
+  return binaryTreeInstance;
+};   
+
+var binaryMethods = {};
+
+binaryMethods.insert = function(value) {
+  var binaryTree = new BinarySearchTree(value);
+  if ( (this.left) === null && binaryTree.value < this.value) {
+    this.left = binaryTree;
+  } else if ((this.right) === null && binaryTree.value > this.value) {
+    this.right = binaryTree;
+  } else if ((this.left) !== null && binaryTree.value < this.value) {
+    this.left.insert(value);
+  } else if ((this.right) !== null && binaryTree.value > this.value) {
+    this.right.insert(value);
+  }
 };
-
-
+binaryMethods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (value < this.value && (this.left)) {
+    return this.left.contains(value);
+  } else if (value > this.value && (this.right)) {
+    return this.right.contains(value);
+  } 
+  return false;
+};
+binaryMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  } if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
