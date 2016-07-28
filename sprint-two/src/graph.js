613c457f586d98cac0fd65d2ac258361f4798a0a
k-rathi <kunalrathi1994@gmail.com>
2016-07-27 21:32:31 -0700

    Completed linkedList and tree

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,47 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodeList = [];
+  this.edgeList = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodeList.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.nodeList.indexOf(node) > -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var index = this.nodeList.indexOf(node);
+  this.nodeList.splice(index, 1);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for(var i = 0 ; i < this.edgeList.length; i++) {
+    if( (this.edgeList[i].contains(node1.fromNode) && this.edgeList[i].contains(node2.toNode) ) ||(this.edgeList[i].contains(node2.fromNode) && this.edgeList[i].contains(node1.toNode) ) ) {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var newEdge = {};
+  newEdge.node1 = fromNode;
+  newEdge.node2 = toNode;
+  this.edgeList.push(newEdge);
 };
 
 // Remove an edge between any two specified (by value) nodes.
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,11 +13,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  var child = new Tree(value);
+  this.children.push(child);  // fix me
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    } 
+  }
+
+  return false;
 };
 
 
