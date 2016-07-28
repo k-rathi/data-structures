1aa58ecd47340c6f6785db43a1460557846f3884
k-rathi <kunalrathi1994@gmail.com>
2016-07-28 11:31:20 -0700

    Completed Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,25 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(index, obj);
+  } else {
+    this._storage.get(index)[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) {
+    return this._storage.get(index)[k];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);  
+  delete this._storage.get(index)[k];
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) > -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  var indexOfItem = this._storage.indexOf(item);
+  if ( indexOfItem > -1) {
+    this._storage.splice(indexOfItem, 1);
+  }
 };
 
 /*
