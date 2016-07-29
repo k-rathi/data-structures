0a61f9e926b753ba8a43698cc8fb2c2ae1cb2e75
k-rathi <kunalrathi1994@gmail.com>
2016-07-28 20:04:53 -0700

    Manipulated Hash to expand on filling storage

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,32 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._expandLimit = this._limit * (3 / 4);
+};
+
+HashTable.prototype.expand = function() {
+  var counter = 0;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] !== undefined) {
+      counter++;
+    }
+  } 
+  return counter;
+};
+
+HashTable.prototype.newHash = function(storage, newLimit) {
+  var newHash = new HashTable();
+  newHash.limit = newLimit;
+  newHash._storage = LimitedArray(newLimit);
+  newHash._expandLimit = newHash._limit * (3 / 4);
+  for (var i = 0; i < storage.length; i++) {
+    for (key in storage[i]) {
+      newHash.insert(key, storage[i][key]);
+    }
+  }
+  this._storage = newHash._storage;
+  this.limit = newHash.limit;
+  this._expandLimit = newHash._expandLimit;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -11,6 +37,9 @@ HashTable.prototype.insert = function(k, v) {
     var obj = {};
     obj[k] = v;
     this._storage.set(index, obj);
+    if ((this.expand + 1) > this._expandLimit) {
+      this.newHash(this._storage, this._expandLimit);
+    }
   } else {
     this._storage.get(index)[k] = v;
   }
